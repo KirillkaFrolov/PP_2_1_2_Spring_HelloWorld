0c0cbba8fd7e06230ae7fbe75b965a54e553a3a8
KirillkaFrolov <kir13russs@mail.ru>
2023-11-22 18:32:41 +0300

    Решение задачи

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        boolean beanSet = bean == bean1;
+        System.out.println(beanSet);
+        boolean catSet = catBean1 == catBean2;
+        System.out.println(catSet);
     }
 }
